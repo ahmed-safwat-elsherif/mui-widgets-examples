094cf352b20ced1dc5ae1bd96291359bdcf7dd40
Ahmed Safwat <a.safwat@tap.company>
2025-05-31 01:06:13 +0300

    Force trigger the deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import {
-  Box,
-  CssBaseline,
-  Divider,
-  GlobalStyles,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { CssBaseline, GlobalStyles } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import ChatApp from "./examples/ChatApp";
-import ContactList from "./examples/ContactList";
-import DashboardWidget from "./examples/DashboardWidget";
-import LoginForm from "./examples/LoginForm";
-import NotificationExample from "./examples/NotificationExample";
-import ProductCard from "./examples/ProductCard";
-import SelectForm from "./examples/SelectForm";
-import SimpleTabs from "./examples/SimpleTabs";
-import MusicPlayer from "./examples/MusicPlayer";
 import { Route, Routes } from "react-router";
 import Task1 from "./pages/Task1";
 import Task2 from "./pages/Task2";
-import Navbar from "./components/Navbar";
 
 const theme = createTheme({});
 
